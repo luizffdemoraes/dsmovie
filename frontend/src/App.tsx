7916ccd46b899aaa5bcb75a2e00227ca63cc3190
Luiz Felipe Moraes <lffm1994@gmail.com>
2022-01-12 00:04:22 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import Navbar from "./components";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Navbar from "components/Navbar";
+import Form from "pages/Form";
 
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
